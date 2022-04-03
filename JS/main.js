40e8a3bb3697852e760d6c42c747bef1c83edf5a
jani-eriksson <72618353+jani-eriksson@users.noreply.github.com>
2022-04-03 10:31:48 +0300

    Added array of objects

--- JS/main.js
+++ JS/main.js
@@ -6,3 +6,18 @@ const food = {
     type: 'Dinner'
 }
 console.log(food.name, food.type);
+
+
+//array of objects
+let dinner = [
+    {
+        name: 'Spinach Crepes',
+        type: 'Dinner'
+    },
+    {
+        name: 'Fish',
+        type: 'Dinner'
+    }
+]
+
+console.log(dinner)

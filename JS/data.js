bee38f7d2efe224762497664e77b986f28549226
giulia-lir <giulia.libe3@gmail.com>
2022-04-18 18:58:16 +0300

    Added data

--- JS/data.js
+++ JS/data.js
@@ -1,4 +1,13 @@
-let food = [
+/*
+{
+    'title': '',
+    'ingredients': [''],
+    'image': ''
+},
+*/
+
+
+let appetizers = [
     {
         'name': 'Spinach Crepes',
         'type': 'Dinner'
@@ -13,18 +22,23 @@ let food = [
     }
 ]
 
-let food2 = [
+let mainCourses = [
     {
         'title': 'Spinach Crepes',
-        'type': 'Main',
         'ingredients': ['2 eggs', '5 dl milk', '2.5 dl flour', '1 bag of frozen spinach', '1 tbs of salt'],
-        'guide': 'Melt spinach',
-        'image': 'https://is.mediadelivery.fi/img/658/aaf82930112c45c7ba566334e85f786b.jpg.webp' //replace
+        'image': 'https://is.mediadelivery.fi/img/658/aaf82930112c45c7ba566334e85f786b.jpg.webp' 
     },
     {
-        'title': 'Spinach Crepes', //duplicate
-        'type': 'Main',
-        'ingredients': ['2 eggs', '5 dl milk', '2.5 dl flour', '1 bag of frozen spinach', '1 tbs of salt'],
-        'guide': 'Melt spinach'
+        'title': 'Teriyaki chicken with broccoli', 
+        'ingredients': ['450g chicken', '2 spoons of sake', '2 spoons of soy sauce', '3 spoons of mirin', '1 broccoli', '1 spoon of oil', 'salt']
+    },
+    {
+        'title': 'Chicken Kiev',
+        'ingredients': ['4 chicken breats', '1dl flour', '1 and half dl of crumblebread', '2 eggs', '3 spoons of oil', '125g butter', '2 garlic cloves', '3 spoons of parsley', 'salt']
     }
+]
+
+let desserts = [
+    {},
+    {}
 ]

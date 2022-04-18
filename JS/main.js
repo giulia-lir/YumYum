dc968c8a12923c5ff9c073fe40eebc15ab380476
giulia-lir <giulia.libe3@gmail.com>
2022-04-18 19:58:42 +0300

    Random and title print working

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,19 @@
-
-console.log(food2)
-
 function printAll(array) {
     let output = "";
     for (let i of array) {
         output += `Name: ${i.name} Type: ${i.type}<br>`;
     }
     document.getElementById('items').innerHTML = output;
+}
+
+
+
+function randomize() {
+    const foodTable = document.getElementsByClassName("food");
+    for(let i = 0; i < 14; i++) {
+        let randomNumber = Math.floor((Math.random() * mainCourses.length));
+        console.log(i + " " + randomNumber)
+        foodTable[i].innerHTML = mainCourses[randomNumber].title;
+    }
+
 }
